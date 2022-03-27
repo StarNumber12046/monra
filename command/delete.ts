efde150e1197bea79827f5d22fa4bcf696a10367
Thebigbot <77632836+Thebigbot0000@users.noreply.github.com>
2022-03-27 09:49:25 +0200

    create

--- command/delete.ts
+++ command/delete.ts
@@ -1,3 +1,16 @@
+import * as fs from "node:fs";
+import { execSync } from "node:child_process";
+import { exit } from "node:process";
+import { question } from "readline-sync";
+
 export const _delete = (x: string) => {
-    
+    const packages = JSON.parse(fs.readFileSync("package.json", "utf8"));
+    const monra = JSON.parse(fs.readFileSync("monra.json", "utf8"));
+    if (monra?.client == "yarn") {
+        execSync("yarn remove "+x[1])
+    }
+
+    if (monra?.client == "npm") {
+        execSync("npm remove "+x[1])
+    }
 }
