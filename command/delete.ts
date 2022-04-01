e271ad7ca24a322e89e30289cdef51abdbc29cad
Thebigbot <77632836+Thebigbot0000@users.noreply.github.com>
2022-04-01 14:13:13 +0200

    command(delete): delete by name (or directory name with -d)

--- command/delete.ts
+++ command/delete.ts
@@ -24,31 +24,64 @@ export const _delete = (x: string) => {
         console.log("\x1b[31m[monra]\x1b[0m  Directory name is undefined (use: monra delete <directory-name>)");
         exit()
     } else {
-        const monra = JSON.parse(fs.readFileSync("monra.json", "utf8"));
-        const packagejs = JSON.parse(fs.readFileSync("package.json", "utf8"));
-
-        const yesorno = question('\x1b[31m[monra]\x1b[0m \x1b[1mAre you sure?\x1b[0m (y/n) ');
-
-        if (yesorno == "y" || yesorno == "yes" || yesorno == "") {
-            const packageint = JSON.parse(fs.readFileSync(monra?.directory+"/"+x[1]+"/package.json", "utf8"));
-            if (monra?.client == "yarn") {
-                execSync("yarn remove "+packageint?.name+" -W")
+        if (x[2] == "-d" || x[2] == "-dir" || x[2] == "--directory") {
+            const monra = JSON.parse(fs.readFileSync("monra.json", "utf8"));
+            const packagejs = JSON.parse(fs.readFileSync("package.json", "utf8"));
+    
+            const yesorno = question('\x1b[31m[monra]\x1b[0m \x1b[1mAre you sure?\x1b[0m (y/n) ');
+    
+            if (yesorno == "y" || yesorno == "yes" || yesorno == "") {
+                const packageint = JSON.parse(fs.readFileSync(monra?.directory+"/"+x[1]+"/package.json", "utf8"));
+                if (monra?.client == "yarn") {
+                    execSync("yarn remove "+packageint?.name+" -W")
+                }
+    
+                if (monra?.client == "npm") {
+                    execSync("npm uninstall "+packageint?.name)
+                }
+    
+                if (monra?.client == "pnpm") {
+                    execSync("pnpm uninstall "+packageint?.name)
+                }
+    
+                fs.rmSync(monra?.directory+"/"+x[1], { recursive: true, force: true });
+                console.log("\x1b[32m[monra]\x1b[0m Delete package!");
             }
-
-            if (monra?.client == "npm") {
-                execSync("npm uninstall "+packageint?.name)
+    
+            if (yesorno == "n" || yesorno == "no") {
+                console.log("\x1b[32m[monra]\x1b[0m Cancel!");
             }
-
-            if (monra?.client == "pnpm") {
-                execSync("pnpm uninstall "+packageint?.name)
+        } else {
+            const monra = JSON.parse(fs.readFileSync("monra.json", "utf8"));
+            const packagejs = JSON.parse(fs.readFileSync("package.json", "utf8"));
+    
+            const yesorno = question('\x1b[31m[monra]\x1b[0m \x1b[1mAre you sure?\x1b[0m (y/n) ');
+    
+            if (yesorno == "y" || yesorno == "yes" || yesorno == "") {
+                const files = fs.readdirSync(monra?.directory)
+                files.map((file) => {
+                    const packageint = JSON.parse(fs.readFileSync(monra?.directory+"/"+file+"/package.json", "utf8"));
+                    if (packageint?.name == x[1]) {
+                    if (monra?.client == "yarn") {
+                        execSync("yarn remove "+packageint?.name+" -W")
+                    }
+        
+                    if (monra?.client == "npm") {
+                        execSync("npm uninstall "+packageint?.name)
+                    }
+        
+                    if (monra?.client == "pnpm") {
+                        execSync("pnpm uninstall "+packageint?.name)
+                    }
+                    fs.rmSync(monra?.directory+"/"+file, { recursive: true, force: true });
+                   }
+                })
+                console.log("\x1b[32m[monra]\x1b[0m Delete package!");
             }
-
-            fs.rmSync(monra?.directory+"/"+x[1], { recursive: true, force: true });
-            console.log("\x1b[32m[monra]\x1b[0m Delete package!");
-        }
-
-        if (yesorno == "n" || yesorno == "no") {
-            console.log("\x1b[32m[monra]\x1b[0m Cancel!");
+    
+            if (yesorno == "n" || yesorno == "no") {
+                console.log("\x1b[32m[monra]\x1b[0m Cancel!");
+            }
         }
     }
 }
